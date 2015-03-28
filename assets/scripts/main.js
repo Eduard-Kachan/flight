79623f027695a9cc2b4e8d4f48c3c8a94617b891
Eduard <eduard.k.kachan@gmail.com>
2015-03-28 18:52:07 -0400

    keyboard controls

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,21 @@
-var camera, scene, renderer, windowWidth, windowHeight, geometry, material;
+var camera, scene, renderer, windowWidth, windowHeight, geometry, material, verticalMovmet, horisontalMovmet;
+var player = {
+    speed: 2,
+    left: false,
+    top: false,
+    right: false,
+    down: false,
+    verticalMovment: 0,
+    horisontalMovment: 0,
+    calculateDirection: function(){
+        if(this.right && !this.left)this.horisontalMovment = -1;
+        if(this.left && !this.right)this.horisontalMovment = 1;
+        if(this.left == this.right)this.horisontalMovment = 0;
+        if(this.up && !this.down)this.verticalMovment = -1;
+        if(this.down && !this.up)this.verticalMovment = 1;
+        if(this.down == this.up)this.verticalMovment = 0;
+    }
+};
 var debris = [];
 var rockGeometrys = [];
 
@@ -28,7 +45,7 @@ function init() {
     windowWidth = window.innerWidth;
     windowHeight = window.innerHeight;
 
-    camera = new THREE.PerspectiveCamera( 40, windowWidth / windowHeight, 1, 1000 );
+    camera = new THREE.PerspectiveCamera( 40, windowWidth / windowHeight, 1, 10000 );
 
     scene = new THREE.Scene();
 
@@ -37,16 +54,17 @@ function init() {
     material = new THREE.MeshLambertMaterial( { color: "red" } );
     createdebris(70);
 
-    var hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+    var hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.9 );
     hemiLight.color.setHSL( 0.6, 1, 0.6 );
     hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
-    hemiLight.position.set( 0, -500, 500git  );
+    hemiLight.position.set( 0, -500, 500);
     scene.add( hemiLight );
 
-    var planetGeo = new THREE.SphereGeometry(400, 15, 15);
+    var planetGeo = new THREE.SphereGeometry(800, 15, 15);
     var planetmaterial = new THREE.MeshLambertMaterial({color:"green"});
     var planet = new THREE.Mesh(planetGeo, planetmaterial);
-    planet.position.z = -1000;
+    planet.scale.z = 0.01;
+    planet.position.z = -1800;
     planet.position.x = -250;
     scene.add(planet);
 
@@ -55,22 +73,43 @@ function init() {
 
 }
 
+window.onkeydown = function(e) {
+    var key = e.keyCode ? e.keyCode : e.which;
+
+    if(key == 37) player.left = true;
+    if(key == 38) player.up = true;
+    if(key == 39) player.right = true;
+    if(key == 40) player.down = true;
+
+    player.calculateDirection();
+};
+
+window.onkeyup = function(e) {
+    var key = e.keyCode ? e.keyCode : e.which;
+
+    if(key == 37) player.left = false;
+    if(key == 38) player.up = false;
+    if(key == 39) player.right = false;
+    if(key == 40) player.down = false;
+
+    player.calculateDirection();
+};
 
 function createdebris(amount) {
     var random;
     for(var i = 0; i < amount; i++){
         random = Math.floor(Math.random() * rockGeometrys.length);
         debris[i] = new THREE.Mesh( rockGeometrys[random], material );
-        debris[i].scale.x = debris[i].scale.y = debris[i].scale.z = 400;
+        debris[i].scale.x = debris[i].scale.y = debris[i].scale.z = 500;
         randomPosition(debris[i]);
         scene.add(debris[i]);
     }
 }
 
 function randomPosition(debris) {
-    debris.position.x = Math.random() * 250;
-    debris.position.y = Math.random() * 200;
-    debris.position.z = -400 - Math.floor(Math.random() * 400);
+    debris.position.x = Math.random() * 400;
+    debris.position.y = Math.random() * 400;
+    debris.position.z = -600 - Math.floor(Math.random() * 600);
 
     if(Math.floor(Math.random() * 2) === 0){debris.position.x *= -1}
     if(Math.floor(Math.random() * 2) === 0){debris.position.y *= -1}
@@ -83,7 +122,7 @@ function randomPosition(debris) {
     if(Math.floor(Math.random() * 2) === 0){debris.userData.finalPosition.y *= -1}
 
     //speed
-    debris.userData.speed = {x:0, y:0, z:Math.random() * 3 + 1};
+    debris.userData.speed = {x:0, y:0, z:Math.random() * 10 + 1};
 
     //distance
     debris.userData.distanceZ = Math.abs(debris.userData.finalPosition.z) + Math.abs(debris.position.z);
@@ -133,12 +172,14 @@ function onMouseMove(e) {
     }
 
     //move camera
-    camera.position.x = horizontal * 40;
-    camera.position.y = -vertical * 40;
+    //camera.position.x = horizontal * 40;
+    //camera.position.y = -vertical * 40;
+    verticalMovmet = vertical * 40;
+    horisontalMovmet = -horizontal * 40;
 
     //rotate camera
-    camera.rotation.y = -horizontal * 5 * Math.PI / 180;
-    camera.rotation.x = -vertical * 5 * Math.PI / 180;
+    //camera.rotation.y = -horizontal * 5 * Math.PI / 180;
+    //camera.rotation.x = -vertical * 5 * Math.PI / 180;
 }
 
 function onWindowResize() {
@@ -153,12 +194,17 @@ function onWindowResize() {
 
 function animate() {
 
+
+
     requestAnimationFrame( animate );
 
+    camera.rotation.y = player.horisontalMovment * 5 * Math.PI / 180;
+    camera.rotation.x = -player.verticalMovment * 5 * Math.PI / 180;
+
     for(var i = 0; i < debris.length; i++){
-        debris[i].position.x += debris[i].userData.speed.x;
-        debris[i].position.y += debris[i].userData.speed.y;
-        debris[i].position.z += debris[i].userData.speed.z;
+        debris[i].position.x += debris[i].userData.speed.x + (player.horisontalMovment * player.speed);
+        debris[i].position.y += debris[i].userData.speed.y + (player.verticalMovment * player.speed);
+        debris[i].position.z += debris[i].userData.speed.z + 2;
 
         debris[i].rotation.x += debris[i].userData.randomRotationSpeed.x;
         debris[i].rotation.y += debris[i].userData.randomRotationSpeed.y;
@@ -169,7 +215,5 @@ function animate() {
         }
     }
 
-
     renderer.render( scene, camera );
-
 }
