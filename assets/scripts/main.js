a035b0715f76734432f1f38bda9cabf4abb8d91c
Eduard <eduard.k.kachan@gmail.com>
2015-03-29 23:42:02 -0400

    ship is flying through asteroids

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,8 @@
-var camera, scene, renderer, windowWidth, windowHeight, planet, geometry, material, verticalMovement, horizontalMovement, t;
+var camera, scene, renderer, windowWidth, windowHeight, planet, geometry, material, materialOutline, verticalMovement, horizontalMovement, t;
 var asteroidColor = 0xBFD7D9;
 var fillLightColor = 0x000000;
 var keyLightColor = 0xE4A268;
-var asteroids = 120;
+var asteroids = 150;
 var player = {
     spaceship: null,
     maxSpeed: 0.4,
@@ -20,33 +20,66 @@ var player = {
         x:0,
         y: 0
     },
+    finalRotation: {
+        x:0,
+        y:180 * Math.PI / 180,
+        z:0
+    },
     calculatePosition: function() {
-        if(this.movement.right && !this.movement.left)this.finalSpeed.x = this.maxSpeed * -1;
-        if(this.movement.left && !this.movement.right)this.finalSpeed.x = this.maxSpeed;
-        if(this.movement.left == this.movement.right)this.finalSpeed.x = 0;
-        if(this.movement.up && !this.movement.down)this.finalSpeed.y = this.maxSpeed * -1;
-        if(this.movement.down && !this.movement.up)this.finalSpeed.y = this.maxSpeed;
-        if(this.movement.down == this.movement.up)this.finalSpeed.y = 0;
+        if(this.movement.right && !this.movement.left){
+            this.finalSpeed.x = -1;
+            this.finalRotation.z = 10 * Math.PI / 180;
+            this.spaceship.rotation.z = 10 * Math.PI / 180;
+        }
+        if(this.movement.left && !this.movement.right){
+            this.finalSpeed.x = 1;
+            this.finalRotation.z = -10 * Math.PI / 180;
+            this.spaceship.rotation.z = -10 * Math.PI / 180;
+        }
+        if(this.movement.left == this.movement.right){
+            this.finalSpeed.x = 0;
+            this.finalRotation.z = 0;
+            this.spaceship.rotation.z = 0;
+        }
+        if(this.movement.up && !this.movement.down){
+            this.finalSpeed.y = -1;
+            this.finalRotation.x = 10 * Math.PI / 180;
+            this.spaceship.rotation.x = 10 * Math.PI / 180;
+        }
+        if(this.movement.down && !this.movement.up){
+            this.finalSpeed.y = 1;
+            this.finalRotation.x = -10 * Math.PI / 180;
+            this.spaceship.rotation.x = -10 * Math.PI / 180;
+        }
+        if(this.movement.down == this.movement.up){
+            this.finalSpeed.y = 0;
+            this.finalRotation.x = 0;
+            this.spaceship.rotation.x = 0;
+        }
     },
     t1:0,
     t2:0,
     update: function(){
         var int = 0.009;
-        if(this.speed.x != this.finalSpeed.x){
-            if(this.speed.x > this.finalSpeed.x){
+        if(this.speed.x != this.finalSpeed.x * this.maxSpeed){
+            if(this.speed.x > this.finalSpeed.x * this.maxSpeed){
                 this.speed.x -= int;
             }else{
                 this.speed.x += int;
             }
         }
 
-        if(this.speed.y != this.finalSpeed.y){
-            if(this.speed.y > this.finalSpeed.y){
+
+
+        if(this.speed.y != this.finalSpeed.y * this.maxSpeed){
+            if(this.speed.y > this.finalSpeed.y * this.maxSpeed){
                 this.speed.y -= int;
             }else{
                 this.speed.y += int;
             }
         }
+
+
 
         this.spaceship.position.x -= this.speed.x;
         this.spaceship.position.y -= this.speed.y;
@@ -57,6 +90,9 @@ var player = {
 };
 
 var debris = [];
+var debrisOutline = [];
+var debris2 = [];
+var debrisOutline2 = [];
 var rockGeometrys = [];
 
 
@@ -98,7 +134,9 @@ function init() {
     var size = 9;
     geometry = new THREE.BoxGeometry( size, size, size );
     material = new THREE.MeshLambertMaterial( { color: asteroidColor } );
-    createDebris(asteroids);
+    materialOutline = new THREE.MeshBasicMaterial( { color: 0xff0000, side: THREE.BackSide, transparent: true, opacity : 0} );
+    createDebris(asteroids, debris, debrisOutline, 800);
+    createDebris(90, debris2, debrisOutline2, 400);
 
     var hemiLight = new THREE.HemisphereLight( 0xC75145, 0xC75145, 0.1);
     hemiLight.position.set( -3, -3, 0);
@@ -152,20 +190,28 @@ window.onkeyup = function(e) {
     player.calculatePosition();
 };
 
-function createDebris(amount) {
+function createDebris(amount, array, array2, area) {
     var random;
     for(var i = 0; i < amount; i++){
         random = Math.floor(Math.random() * rockGeometrys.length);
-        debris[i] = new THREE.Mesh( rockGeometrys[random], material );
-        debris[i].scale.x = debris[i].scale.y = debris[i].scale.z = 800;
-        randomPosition(debris[i]);
-        scene.add(debris[i]);
+        array[i] = new THREE.Mesh( rockGeometrys[random], material );
+        array[i].scale.multiplyScalar(900);
+        randomPosition(array[i], area);
+
+        scene.add(array[i]);
+
+        array2[i] = new THREE.Mesh(
+            rockGeometrys[random].clone(),
+            new THREE.MeshBasicMaterial( { color: 0xff0000, side: THREE.BackSide, transparent: true, opacity : 0} ));
+        array2[i].scale.multiplyScalar(940);
+        array2[i].material.opacity = 0;
+        scene.add(array2[i]);
     }
 }
 
-function randomPosition(debris) {
-    debris.position.x = Math.random() * 800;
-    debris.position.y = Math.random() * 800;
+function randomPosition(debris, area) {
+    debris.position.x = Math.random() * area;
+    debris.position.y = Math.random() * area;
     debris.position.z = -1600;//-600 - Math.floor(Math.random() * 600);
 
     if(Math.floor(Math.random() * 2) === 0){debris.position.x *= -1}
@@ -263,24 +309,52 @@ function animate() {
 
     requestAnimationFrame( animate );
 
-    //camera.rotation.y = player.horisontalMovment * 5 * Math.PI / 180;
-    //camera.rotation.x = -player.verticalMovment * 5 * Math.PI / 180;
-
     for(var i = 0; i < debris.length; i++){
-        debris[i].position.x += debris[i].userData.speed.x/6; // + player.speed.x;
-        debris[i].position.y += debris[i].userData.speed.y/6; // + player.speed.y;
-        debris[i].position.z += debris[i].userData.speed.z/2; // + player.maxSpeed/2;
 
 
-        debris[i].rotation.x += debris[i].userData.randomRotationSpeed.x;
-        debris[i].rotation.y += debris[i].userData.randomRotationSpeed.y;
-        debris[i].rotation.z += debris[i].userData.randomRotationSpeed.z;
+        updateDebris(debris[i], debrisOutline[i]);
 
         if(debris[i].position.z > 2){
-            randomPosition(debris[i]);
+            randomPosition(debris[i], 800);
+            debrisOutline[i].material.opacity = 0;
         }
     }
 
+    for(var i = 0; i < debris2.length; i++){
+
+        updateDebris(debris2[i], debrisOutline2[i]);
+
+        if(debris2[i].position.z > 2){
+            randomPosition(debris2[i], 400);
+            debrisOutline2[i].material.opacity = 0;
+        }
+    }
+
+
     renderer.render( scene, camera );
 
+}
+
+function updateDebris (debri, debriOutline) {
+    //console.log(debri.position);
+
+    debri.position.x += debri.userData.speed.x/6; // + player.speed.x;
+    debri.position.y += debri.userData.speed.y/6; // + player.speed.y;
+    debri.position.z += debri.userData.speed.z/2; // + player.maxSpeed/2;
+
+    debriOutline.position.x = debri.position.x;
+    debriOutline.position.y = debri.position.y;
+    debriOutline.position.z = debri.position.z;
+
+    debri.rotation.x += debri.userData.randomRotationSpeed.x;
+    debri.rotation.y += debri.userData.randomRotationSpeed.y;
+    debri.rotation.z += debri.userData.randomRotationSpeed.z;
+
+    debriOutline.rotation.x = debri.rotation.x;
+    debriOutline.rotation.y = debri.rotation.y;
+    debriOutline.rotation.z = debri.rotation.z;
+
+    if(debri.position.z > -200){
+        debriOutline.material.opacity = 1;
+    }
 }
